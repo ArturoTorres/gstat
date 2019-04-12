67ad08d7147dc1a1b49585be28ad032bcc6555a1
Benedikt Gräler <b.graeler@52north.org>
2019-04-12 21:11:05 +0200

    fix build

--- R/zzz.R
+++ R/zzz.R
@@ -11,9 +11,9 @@
 ### pre-NAMESPACE VERSION:
 ## ".First.lib" <-
 ## function(lib, pkg) {
-## 	require(lattice)
-## 	library.dynam("gstat", pkg, lib)
-## 	.Call(gstat_init, as.integer(1))
+##	require(lattice)
+##	library.dynam("gstat", pkg, lib)
+##	.Call(gstat_init, as.integer(1))
 ## }
- 
-variogram <- function(object, ...) UseMethod("variogram")
+
+variogram <- function(object, ...) {UseMethod("variogram")}
